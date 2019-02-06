9f84ab5906e078856af4fcc9f36ac915649f254c
bhatia-kunal <kunal.bhatia@successive.tech>
2019-02-06 16:07:06 +0530

    docs:Intro to Node.js & NPM

---
+++ Extra/patterns/diamond.js
@@ -0,0 +1,28 @@
+const diamond = (rows) => {
+    let str = "";
+    for (let i = 1; i <= rows; i++) {
+        let str = "";
+        for (let j = 1; j <= rows; j++) {
+                if(j <= (rows - i)) {
+                    str += " ";
+                } else {
+                    str += "* "
+                }
+            } 
+            console.log(str);
+            str = "";
+        }
+    for(let i = 1; i <= rows; i++) {
+        for(let j = 1; j <= rows; j++) {
+            if(j >= i) {
+                str += "* ";
+            } else {
+                str+=" ";
+            }
+        }
+        console.log(str);
+        str = "";
+    }
+}
+
+diamond(5);
---
+++ Extra/patterns/equilateralTriangle.js
@@ -0,0 +1,17 @@
+const equilateralTriangle = (rows) => {
+    for (let i = 1; i <= rows; i++) {
+        let str = "";
+        for (let j = 1; j <= rows; j++) {
+                if(j <= (rows - i)) {
+                    str += " ";
+                } else {
+                    str += "* "
+                }
+            } 
+            console.log(str);
+            str = "";
+        }
+    }
+
+
+equilateralTriangle(10);
