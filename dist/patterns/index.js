915e5e8dcf9b33e9481649edc25d5ca4254841d9
bhatia-kunal <kunal.bhatia@successive.tech>
2019-02-07 18:09:23 +0530

    docs:Advantages of TypeScript

--- dist/patterns/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const diamond_1 = require("./diamond");
-const equilateralTriangle_1 = require("./equilateralTriangle");
-exports.default = { diamond: diamond_1.default, equilateralTriangle: equilateralTriangle_1.default };
-//# sourceMappingURL=index.js.map
